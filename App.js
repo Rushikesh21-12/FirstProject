76791c846a1883ce0fab1a5e7169030a33a6b5c7
Rushi <rushisolanki6@gmail.com>
2021-12-02 15:02:50 +0530

    code edited

--- App.js
+++ App.js
@@ -72,7 +72,7 @@ const App: () => Node = () => {
           }}>
           <Section title="Step One">
             Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edit. (Successfully Edited)
+            screen and then come back to see your edit. (Successfully edited code)
           </Section>
           <Section title="See Your Changes">
             <ReloadInstructions />
